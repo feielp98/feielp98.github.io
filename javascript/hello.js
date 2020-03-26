6087498d54a39f20311c259efb7f6c08d9742cb3
Pia Feiel <pia@Pias-Air.fritz.box>
2020-03-26 16:09:08 +0100

    Fehler?

--- javascript/hello.js
+++ javascript/hello.js
@@ -15,7 +15,7 @@ let firstname = "John";
 let lastname = "Smith";
 console.log("Name: ", firstname, lastname)
 
-let fullname = 'Jeffrey "The Dude" Lebowski');
+let fullname = ('Jeffrey "The Dude" Lebowski');
 console.log(fullname);
 
 let template = 'Dein Highscore sind $(highscore) Punkte';
