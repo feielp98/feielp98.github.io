d81132463153c1e54f49bb6e08afb269be01046a
Pia Feiel <pia@Pias-Air.fritz.box>
2020-03-27 17:45:39 +0100

    Fehler gefunden und behoben!!! :D

--- nz/main.js
+++ nz/main.js
@@ -7,4 +7,4 @@ var mymap = L.map(map).setView([-45.8909481029, 170.6417191], 13);
 L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
     maxZoom: 17,
     attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>tributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https:/ntopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-});.addTo(mymap);
+}).addTo(mymap);
