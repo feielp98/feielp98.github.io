955e6d03c690e9241f87a6cb2a361458ec7e6802
Pia Feiel <pia@Pias-Air.fritz.box>
2020-04-02 17:59:13 +0200

    Kreise in eigene Gruppe zeichnen

--- world/main.js
+++ world/main.js
@@ -1,12 +1,13 @@
-
 let startLayer = L.tileLayer.provider("OpenTopoMap")
 let map = L.map("map", {
-    center: [0,0],
+    center: [0, 0],
     zoom: 2,
     layers: [
         startLayer
     ]
 });
+
+let circleGroup = L.featureGroup().addTo(map);
 
 L.control.layers({
     "OpenTopoMap": startLayer,
@@ -15,39 +16,41 @@ L.control.layers({
     "Thunderforest.OpenCycleMap": L.tileLayer.provider("Thunderforest.OpenCycleMap"),
     "CyclOSM": L.tileLayer.provider("CyclOSM"),
     "OpenMapSurfer.Roads": L.tileLayer.provider("OpenMapSurfer.Roads")
+}, {
+    "Thematische Darstellung": circleGroup
+}).addTo(map);
 
-}).addTo(map)
-
-// L.marker([0,0]).addTo(map);
-
-
-//console.log(CONFIRMED);
-
-
-//Mit for-Schleife alle Marker aus dem Datenfile zur Karte hinzufügen
-for (let i = 1; i < CONFIRMED.length; i++) {
-    let row = CONFIRMED[i];
-    //console.log(row[2],row[3]);
-    let reg = `${row[2],row[3]}`;
-    let lat = row[2];
-    let lng = row[3];
-
-    let val = row[row.length-1];
-    // let mrk = L.marker([row[2], row[3]]).addTo(map);
-    // mrk.bindPopup(`${row[0]} ${row[1]}:${val}`);
-
-    //Kreis-Symbole berechnen (Flächengröße)
-    //A=r2*Pi
-    //r=WURZEL(A/Pi)
-    let s =0.25
-    let r = Math.sqrt(val*s/Math.PI);
-    
-
-    let circle = L.circleMarker([lat, lng], {
-        radius: r
-
-    }).addTo(map);
-    circle.bindPopup(`${reg}: ${val}`);
-}
+let drawCircles = funtion(data) {
+    // L.marker([0,0]).addTo(map);
 
 
+    //console.log(CONFIRMED);
+
+
+    //Mit for-Schleife alle Marker aus dem Datenfile zur Karte hinzufügen
+    for (let i = 1; i < CONFIRMED.length; i++) {
+        let row = CONFIRMED[i];
+        //console.log(row[2],row[3]);
+        let reg = `${row[2],row[3]}`;
+        let lat = row[2];
+        let lng = row[3];
+
+        let val = row[row.length - 1];
+        // let mrk = L.marker([row[2], row[3]]).addTo(map);
+        // mrk.bindPopup(`${row[0]} ${row[1]}:${val}`);
+
+        //Kreis-Symbole berechnen (Flächengröße)
+        //A=r2*Pi
+        //r=WURZEL(A/Pi)
+        let s = 0.25
+        let r = Math.sqrt(val * s / Math.PI);
+
+
+        let circle = L.circleMarker([lat, lng], {
+            radius: r
+
+        }).addTo(circleGroup);
+        circle.bindPopup(`${reg}: ${val}`);
+    }
+
+};
