30bad3dd2981f3b5d8221762ebf5ec8ba791308a
Pia Feiel <pia@Pias-Air.fritz.box>
2020-04-02 17:47:05 +0200

    Werte als Kreise (nach Fläche) darstellen

--- world/main.js
+++ world/main.js
@@ -28,9 +28,26 @@ L.control.layers({
 for (let i = 1; i < CONFIRMED.length; i++) {
     let row = CONFIRMED[i];
     //console.log(row[2],row[3]);
+    let reg = `${row[2],row[3]}`;
+    let lat = row[2];
+    let lng = row[3];
+
     let val = row[row.length-1];
-    let mrk = L.marker([row[2], row[3]]).addTo(map);
-    mrk.bindPopup(`${row[0]} ${row[1]}:${val}`);
+    // let mrk = L.marker([row[2], row[3]]).addTo(map);
+    // mrk.bindPopup(`${row[0]} ${row[1]}:${val}`);
+
+    //Kreis-Symbole berechnen (Flächengröße)
+    //A=r2*Pi
+    //r=WURZEL(A/Pi)
+    let s =0.25
+    let r = Math.sqrt(val*s/Math.PI);
+    
+
+    let circle = L.circleMarker([lat, lng], {
+        radius: r
+
+    }).addTo(map);
+    circle.bindPopup(`${reg}: ${val}`);
 }
 
 
