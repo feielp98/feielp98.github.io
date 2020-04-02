ee73c50f7c33e2acc54562fdf69d3be162be738a
Pia Feiel <pia@Pias-Air.fritz.box>
2020-04-02 16:16:22 +0200

    Neuseeland Karte

--- nz/main.js
+++ nz/main.js
@@ -10,12 +10,12 @@ let map = document.querySelector("#map")
 
 var mymap = L.map(map).setView([-45.8909481029, 170.6417191], 13);
 
-// L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-//     maxZoom: 17,
-//     attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>tributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https:/ntopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-// }).addTo(mymap);
+L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+    maxZoom: 17,
+    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>tributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https:/ntopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+}).addTo(mymap);
 
-L.tileLayer.provider('Stamen.Watercolor').addTo(map);
+// L.tileLayer.provider('Stamen.Watercolor').addTo(map);
 
 var marker = L.marker([-45.8909481029, 170.6417191]).addTo(mymap);
 
