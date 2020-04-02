0107cd38345ad72524b9acf0cdee0bdf2b80870c
Pia Feiel <pia@Pias-Air.fritz.box>
2020-04-02 16:58:52 +0200

    Pushen

--- world/main.js
+++ world/main.js
@@ -1,7 +1,19 @@
+
+let startLayer = L.tileLayer.provider("OpenTopoMap")
 let map = L.map("map", {
     center: [0,0],
     zoom: 2,
     layers: [
-        L.tileLayer.provider("OpenTopoMap")
+        startLayer
     ]
 });
+
+L.control.layers({
+    "OpenTopoMap": startLayer,
+    "OpenStreetMap.Mapnik": L.tileLayer.provider("OpenStreetMap.Mapnik"),
+    // "Stadia.OSMBright": L.tileLayer.provider("Stadia.OSMBright"),
+    // "Thunderforest.OpenCycleMap": L.tileLayer.provider("Thunderforest.OpenCycleMap"),
+    // "CyclOSM": L.tileLayer.provider("CyclOSM"),
+
+}).addTo(map)
+
