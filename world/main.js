f45b975b8b904f3e026ede83bbd186bceee689e1
Pia Feiel <pia@Pias-Air.fritz.box>
2020-04-09 15:50:53 +0200

    Datum der Werte im header anzeigen

--- world/main.js
+++ world/main.js
@@ -25,6 +25,9 @@ let drawCircles = function () {
     let header = CONFIRMED[0];
     let index = header.length - 1;
 
+    // Datum anzeigen
+    document.querySelector("#datum").innerHTML = `am ${header[index]}`;
+    
     //console.log(data);
     for (let i = 1; i < data.length; i++) {
         let row = data[i];
