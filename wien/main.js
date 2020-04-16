eb352c5fb61c35c4248723d80243b54866a2c949
Pia Feiel <pia@Pias-Air.fritz.box>
2020-04-16 17:11:50 +0200

    Linien und Flächen einbinden und popups

--- wien/main.js
+++ wien/main.js
@@ -50,3 +50,25 @@ walk.on("data:loaded", function(){
     map.fitBounds(walkGroup.getBounds());
 });
 // map.fitBounds(walkGroup.getBounds());
+
+let wandern = "https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:WANDERWEGEOGD&srsName=EPSG:4326&outputFormat=json";
+
+L.geoJson.ajax(wandern, {
+    style: function(){
+      return { color:"green", weight: 5};  
+    }
+}).addTo(map);
+
+let heritage = "https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:WELTKULTERBEOGD&srsName=EPSG:4326&outputFormat=json";
+
+L.geoJson.ajax(heritage,{
+    style:function(){
+        return{color:"salmon", fillOpacity: 0.3}
+    },
+    onEachFeature: function(feature, layer){
+        console.log("Feature", feature);
+        layer.bindPopup(`<h3>${feature.properties.NAME}</h3>
+        <p>${feature.properties.INFO}</p>
+        `)
+    }
+}).addTo(map);
