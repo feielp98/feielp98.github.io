be8c09c0f9efc14b76570b799cec549f17afa1ad
Pia Feiel <pia@Pias-Air.fritz.box>
2020-04-30 16:30:24 +0200

    Popup

--- adlerweg/main.js
+++ adlerweg/main.js
@@ -8,6 +8,10 @@ let map = L.map("map", {
     ]
 });
 
+let overlay={
+    adlerblicke: L.featureGroup()
+};
+
 L.control.layers({
     "BasemapAT.grau": L.tileLayer.provider("BasemapAT.grau"),
     "BasemapAT": L.tileLayer.provider("BasemapAT"),
@@ -20,6 +24,32 @@ L.control.layers({
         L.tileLayer.provider("BasemapAT.orthofoto"),
         L.tileLayer.provider("BasemapAT.overlay")
     ])
+}, {
+    "Adlerblicke": overlay.adlerblicke
 }).addTo(map);
 
-console.log(ADLERBLICKE); //schauen, ob Etappen-File eingebunden wurde
+// console.log(ADLERBLICKE); //schauen, ob Etappen-File eingebunden wurde
+
+
+for (const blick of ADLERBLICKE) {
+    console.log(blick);
+    let mrk = L.marker([blick.lat,blick.lng]).addTo(map);
+    mrk.bindPopup(`Standort ${blick.standort} (${blick.seehoehe}m)`);
+};
+overlay.adlerblicke.addTo(map);
+
+
+// pointToLayer: function(feature, latlng) {
+//     return L.marker(latlng, {
+//         title: `${feature.properties.standort} (${feature.geometry.coordinates[2]}m)`,
+//         icon: L.divIcon({
+//             html: `<div class="label-temperature" style="background-color:${color}">${feature.properties.LT.toFixed(1)}</div>`,
+//             className: "ignore-me" // dirty hack
+//         }
+
+
+// onEachFeature: function (feature, layer){
+//     console.log("Wanderweg Feature", feature);
+//     layer.bindPopup(`${feature.properties.standort} ${feature.properties.seehoehe}`)
+// }
+// }).addTo(map)
