f7041faac3a83ef73eea9bcd767e35fd8cff6a7f
Pia Feiel <pia@Pias-Air.fritz.box>
2020-04-30 18:41:01 +0200

    Bug beheben (titel im pulldown)

--- adlerweg/main.js
+++ adlerweg/main.js
@@ -91,7 +91,7 @@ drawEtappe(1);
 let pulldown = document.querySelector("#pulldown"); // in index.html erzeugtes Pulldown ansprechen
 console.log(pulldown);
 
-for (let i = 0; i < ETAPPEN.length; i++) {
+for (let i = 1; i < ETAPPEN.length; i++) {
     const etappe = ETAPPEN[i];
     console.log(etappe);
     pulldown.innerHTML += `<option value ="${i}">${etappe.titel}</option>`; 
