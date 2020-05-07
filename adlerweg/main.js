06742c62b9005fafe6afcfaa3e3d3d8418a45713
Pia Feiel <pia@Pias-Air.fritz.box>
2020-05-07 18:20:04 +0200

    icons für Wikipedia-Inhalte

--- adlerweg/main.js
+++ adlerweg/main.js
@@ -8,7 +8,7 @@ let map = L.map("map", {
     ]
 });
 
-let overlay={
+let overlay = {
     adlerblicke: L.featureGroup(),
     etappen: L.featureGroup(),
     einkehr: L.featureGroup(),
@@ -39,7 +39,7 @@ L.control.layers({
 
 for (const blick of ADLERBLICKE) {
     // console.log(blick);
-    let mrk = L.marker([blick.lat,blick.lng], {
+    let mrk = L.marker([blick.lat, blick.lng], {
         icon: L.icon({
             iconSize: [32, 37], //automatisches zentrieren, wenn Bildgröße bekannt
             iconAnchor: [16, 37], //um Bild zu zentrieren
@@ -52,17 +52,17 @@ for (const blick of ADLERBLICKE) {
 }
 overlay.adlerblicke.addTo(map);
 
-let drawEtappe = function(nr){
+let drawEtappe = function (nr) {
     overlay.etappen.clearLayers(); // bestehende Etappe löschen
 
     console.log(ETAPPEN[nr].track);
-    let track = ETAPPEN[nr].track.replace("A",""); // A mit nichts ersetzen (bei Benennung in track-Attribute)
+    let track = ETAPPEN[nr].track.replace("A", ""); // A mit nichts ersetzen (bei Benennung in track-Attribute)
     console.log(track);
 
     let gpx = new L.GPX(`gpx/AdlerwegEtappe${track}.gpx`, {
         async: true,
         marker_options: {
-            startIconUrl: `icons/number_${nr}.png`, 
+            startIconUrl: `icons/number_${nr}.png`,
             endIconUrl: 'icons/finish.png',
             shadowUrl: null, // eigene Grafik die Schatten macht, brauchen wir nicht deshalb null
             iconSize: [32, 37],
@@ -71,11 +71,11 @@ let drawEtappe = function(nr){
         },
         polyline_options: {
             color: "black",
-            dashArray: [2,5]
+            dashArray: [2, 5]
         }
     });
-    
-    gpx.on("loaded", function(evt){
+
+    gpx.on("loaded", function (evt) {
         map.fitBounds(evt.target.getBounds());
 
         controlElevation.clear(); //damit nicht immer dazugezeichnet wird, zuerst bestehendes Profil löschen
@@ -85,12 +85,12 @@ let drawEtappe = function(nr){
 
     //Schleife geht alle Infos des Objekts durch (zuvor im Index.html beschrieben) -> um Texte entsprechend der Etappe anzupassen
     for (const key in ETAPPEN[nr]) {
-            const element = ETAPPEN[nr][key];
-            console.log(`et-${key}`);
-            let elem = document.querySelector(`#et-${key}`);
-            if (elem) {
-                elem.innerHTML = ETAPPEN[nr][key];
-            }
+        const element = ETAPPEN[nr][key];
+        console.log(`et-${key}`);
+        let elem = document.querySelector(`#et-${key}`);
+        if (elem) {
+            elem.innerHTML = ETAPPEN[nr][key];
+        }
     }
 };
 drawEtappe(1);
@@ -101,28 +101,28 @@ console.log(pulldown);
 for (let i = 1; i < ETAPPEN.length; i++) {
     const etappe = ETAPPEN[i];
     console.log(etappe);
-    pulldown.innerHTML += `<option value ="${i}">${etappe.titel}</option>`; 
+    pulldown.innerHTML += `<option value ="${i}">${etappe.titel}</option>`;
 }
 // Etappenpulldown aktivieren (über klick ändern sich die angezeigten Etappen)
-pulldown.onchange = function(evt) {
+pulldown.onchange = function (evt) {
     let nr = evt.target.options[evt.target.options.selectedIndex].value;
     //console.log(nr);
     drawEtappe(nr);
 }
 
 
-let drawEinkehr = function (){
+let drawEinkehr = function () {
     for (let einkehr of EINKEHR) {
         console.log(einkehr);
-        let mrk = L.marker([einkehr[2], einkehr [3]],{
+        let mrk = L.marker([einkehr[2], einkehr[3]], {
             icon: L.icon({
                 iconSize: [32, 37], //automatisches zentrieren, wenn Bildgröße bekannt
                 iconAnchor: [16, 37], //um Bild zu zentrieren
                 popupAnchor: [0, -37], //37 Pixel nach oben verschieben
                 iconUrl: "icons/restaurant.png"
             })
-            }).addTo(overlay.einkehr);
-            mrk.bindPopup(`${einkehr[1]} (Etappe ${einkehr[0]})`) //zeigt Spalte 2 (1) und 1 (0) an
+        }).addTo(overlay.einkehr);
+        mrk.bindPopup(`${einkehr[1]} (Etappe ${einkehr[0]})`) //zeigt Spalte 2 (1) und 1 (0) an
 
     }
 };
@@ -140,7 +140,7 @@ L.control.scale({
     imperial: false //nicht-metrische Werte weglassen
 }).addTo(map); //erzeugt einen Maßstab links unten bei der Karte
 
-map.on("zoomend moveend", function (evt){
+map.on("zoomend moveend", function (evt) {
     let ext = {
         north: map.getBounds().getNorth(),
         south: map.getBounds().getSouth(),
@@ -150,45 +150,46 @@ map.on("zoomend moveend", function (evt){
     let url = `https://secure.geonames.org/wikipediaBoundingBoxJSON?north=${ext.north}&south=${ext.south}&east=${ext.east}&west=${ext.west}&username=feielp98&lang=de&maxRows=30`;
     console.log(url);
 
-    let wiki = L.Util.jsonp(url).then(function(data){
+    let wiki = L.Util.jsonp(url).then(function (data) {
         //console.log(data.geonames);
-        for (let article of data.geonames){
-            let png ="";
+        for (let article of data.geonames) {
+            let png = "";
             // console.log(article.feature)
-            switch (article.feature){
+            switch (article.feature) {
                 case "city":
-                    png="city.png";
+                    png = "city.png";
                     break;
                 case "landmark":
-                    png="landmark.png";
+                    png = "landmark.png";
                     break;
                 case "waterbody":
-                    png="lake.png";
+                    png = "lake.png";
                     break;
                 case "river":
-                    png="river.png";
+                    png = "river.png";
                     break;
                 case "mountain":
-                    png="mountains.png";
+                    png = "mountains.png";
                     break;
                 default:
-                    png="information.png";
+                    png = "information.png";
             }
             console.log(png);
 
-            let mrk = L.marker([article.lat,article.lng], {
-                // icon: L.icon({
-                //     iconSize: [32, 37], //automatisches zentrieren, wenn Bildgröße bekannt
-                //     iconAnchor: [16, 37], //um Bild zu zentrieren
-                //     popupAnchor: [0, -37], //37 Pixel nach oben verschieben
-                //     iconUrl: "icons/restaurant.png"
+            let mrk = L.marker([article.lat, article.lng], {
+                icon: L.icon({
+                    iconSize: [32, 37],
+                    iconAnchor: [16, 37],
+                    popupAnchor: [0, -37],
+                    iconUrl: `icons/${png}`
+                })
             }).addTo(overlay.wikipedia);
             mrk.bindPopup(`
-                <small>${article.feature}</small>
-                <h3>${article.title} (${article.elevation}m)</h3>
-                <p>${article.summary}</p>
-                <a target="wikipedia" href="https://${article.wikipediaUrl}">Wikipedia Artikel</a>
-            `)
+                        <small>${article.feature}</small>
+                        <h3>${article.title} (${article.elevation}m)</h3>
+                        <p>${article.summary}</p>
+                        <a target="wikipedia" href="https://${article.wikipediaUrl}">Wikipedia Artikel</a>
+                    `)
             //console.log(article);
         }
     });
